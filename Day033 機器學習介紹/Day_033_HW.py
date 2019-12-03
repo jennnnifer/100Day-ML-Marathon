53461f33a5f4d03e61640b5f616f79de5ecdaf48
Jennifer Wu <jennifer.wu@shopee.com>
2019-12-03 19:44:58 +0800

    Create Day_033_HW.py

---
+++ Day033 機器學習介紹/Day_033_HW.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 練習時間
+
+# 請觀看李宏毅教授以神奇寶貝進化 CP 值預測的範例，解說何謂機器學習與過擬合。並回答以下問題
+
+# [youtube](https://www.youtube.com/watch?v=fegAeph9UaA)
+
+# ### 1. 模型的泛化能力 (generalization) 是指什麼？ 
+# ### 2. 分類問題與回歸問題分別可用的目標函數有哪些？
+
+# In[ ]:
+
+
+# 1. 泛化能力是指機器的學習能力, 算是過擬合的反指標, 泛化能力越強越適合做分群
+# 2. 分類問題常用: cross entropy, 回歸問題常用: MSE, MAE
+
