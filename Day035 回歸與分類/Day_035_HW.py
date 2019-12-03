1bebd82ec3f582505f4fd6153efcfaed1debf92a
Jennifer Wu <jennifer.wu@shopee.com>
2019-12-03 19:57:20 +0800

    Create Day_035_HW.py

---
+++ Day035 回歸與分類/Day_035_HW.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 練習時間
+# 相信大家對回歸問題及分類問題都有初步的了解。今天的作業希望大家搜尋有關 multi-label 的案例問題。下圖是電影 "奇異博士" 的分類。可以看到同時有 "Action", "Adventure", "Fantasy" 的類別存在，代表這是個多標籤的問題。 
+
+# ![image](https://cdn-images-1.medium.com/max/1000/1*r0gYXMSQf5VhdMyl2bRDyg.png)
+
+# ## 請搜尋目標為多標籤問題 (Multi-label) 的機器學習案例，了解其資料來源、目標以及評估指標為何
+# ## Hint: 服飾
