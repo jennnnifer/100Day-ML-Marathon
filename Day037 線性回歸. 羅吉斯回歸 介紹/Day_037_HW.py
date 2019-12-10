9071dc8380b54feb57e703a059d2b44e1aabfda6
Jennifer Wu <jennifer.wu@shopee.com>
2019-12-10 19:42:42 +0800

    Create Day_037_HW.py

---
+++ Day037 線性回歸. 羅吉斯回歸 介紹/Day_037_HW.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## [作業重點]
+# 了解線性回歸的模型發展歷程，並了解優勢與劣勢，以及其使用情境
+
+# ## 作業
+
+# 請閱讀以下相關文獻，並回答以下問題
+# 
+# [Linear Regression 詳細介紹](https://brohrer.mcknote.com/zh-Hant/how_machine_learning_works/how_linear_regression_works.html)
+# 
+# [Logistics Regression 詳細介紹](https://medium.com/@yehjames/%E8%B3%87%E6%96%99%E5%88%86%E6%9E%90-%E6%A9%9F%E5%99%A8%E5%AD%B8%E7%BF%92-%E7%AC%AC3-3%E8%AC%9B-%E7%B7%9A%E6%80%A7%E5%88%86%E9%A1%9E-%E9%82%8F%E8%BC%AF%E6%96%AF%E5%9B%9E%E6%AD%B8-logistic-regression-%E4%BB%8B%E7%B4%B9-a1a5f47017e5)
+# 
+
+# 1. 線性回歸模型能夠準確預測非線性關係的資料集嗎?
+# 2. 回歸模型是否對資料分布有基本假設?
+# 
+
+# ANS: 
+# 1. 還是可以預測, 只是準確度不高
+# 2. 有, 例如Logistics Regression假設數據符合百努力分布
+
+# In[ ]:
+
+
+
+
